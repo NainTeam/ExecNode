54f8974b235a76a4ff06f6e539592abc061cad9e
Eslem <j.eslem03@gmail.comm>
2016-02-04 06:24:45 +0100

    init

---
+++ ExecApi/routes/exec.ts
@@ -0,0 +1,15 @@
+﻿var sys = require('sys')
+var exec = require('child_process').exec;
+/*
+ * GET home page.
+ */
+import express = require('express');
+
+export function index(req: express.Request, res: express.Response) {    
+    exec("whoami", (error, stdout, stderr) => res.send('Loggued as:'+ stdout));
+};
+
+export function execute(req: express.Request, res: express.Response) {
+    console.log(req.body);
+    exec(req.body.exec, (error, stdout, stderr) => res.send('Command:\n\t' + stdout));
+};
---
+++ ExecApi/routes/index.ts
@@ -0,0 +1,6 @@
+﻿import express = require('express');
+
+export function index(req: express.Request, res: express.Response) {
+
+    res.send('Exec api');
+};
